a36c1ddb4991e907967ab5bbe260e5be8fc9321f
Marcus Aurén <auren@student.chalmers.se>
2016-02-25 14:32:09 +0100

    Added java-files and gitignore

---
+++ src/CompDijkstraPath.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Marcus on 2016-02-25.
+ * Class is used for comparing spans using Dijkstras algorithm
+ */
+public class CompDijkstraPath {
+    //todo implement
+}
---
+++ src/CompKruskalEdge.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Marcus on 2016-02-25.
+ * Class is used for comparing edges using Kruskals algorithm
+ */
+
+public class CompKruskalEdge {
+    //todo implement
+
+}
