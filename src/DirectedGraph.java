91711d1f0e2f3c54eaa2e8c897cf8b276f8e1143
Marcus Aurén <auren@student.chalmers.se>
2016-02-25 14:53:28 +0100

    work has begun

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -2,14 +2,15 @@
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
-
-
+    ArrayList nodList;
 	public DirectedGraph(int noOfNodes) {
-		;
-	}
+		nodList = new ArrayList(noOfNodes);
+    }
 
 	public void addEdge(E e) {
-		;
+        if(e != null) {
+            nodList.add(e);
+        }
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
